bc14f9133805e5e3e043b3bf95ea98c624e766a3
Anton Tetov <anton@tetov.se>
2023-06-05 11:31:14 +0200

    tweaks steps/sec

--- firmware/simple-stepper/simple-stepper.ino
+++ firmware/simple-stepper/simple-stepper.ino
@@ -1,57 +1,67 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
 
-//define pins
-const int O_PIN_PULSE = 9; // PUL - Pulse
-const int O_PIN_DIRECTION = 7;  // DIR - Direction
-const int I_PIN_RUN = 2;
+// PINS
+const int DO_STEP_PIN = 9;
+const int DO_DIR_PIN = 7;
+const int DI_ROBOT_STEPPER_FORWARDS_PIN = 2;
 
-const int EXTRUDER_RPM = 120;
+// const int DI_ROBOT_ESTOP_PIN = 2;
+// const int DO_STEPPER_ENABLE_PIN = 2;
+
+// MOTOR
+
+const int EXTRUDER_RPM = 20;
 
 // NEMA 17HS1070-C5X: 1.8 degrees step angle
-const float STEP_ANGLE = 1.8;
+const float STEP_ANGLE_DEGREES = 1.8;
 
 // microsteps per rev => microstep multiplier:
 // steps_per_rev_microstepping => steps_per_rev_microstepping/steps_per_rev = microstep multiplier
 // e.g:
 // 400 => 400/200 = 2
 // or (steps_per_rev/microsteps_per_rev)^-1 = microstep_multiplier
-const int MICROSTEP_MULTIPLIER = 1;
+const int MICROSTEP_MULTIPLIER = 8;
 
 // NEMA 17HS1070-C5X: 5:1
-const int GEAR_MULTIPLIER = 5;
-
-int STEPS_PER_SEC;
+const float GEAR_RATIO = 1 / 5;
 
 // "Speeds of more than 1000 steps per second are unreliable." --accelstepper docs
 // But it still seems fine
 const float MAX_STEPS_PER_SEC = 1000;
 
-AccelStepper stepper = AccelStepper(AccelStepper::DRIVER, O_PIN_PULSE, O_PIN_DIRECTION);
+const bool STEPPER_INVERT_DIRECTION = false;
 
-void setup()
-{
-  STEPS_PER_SEC = RPM_TO_STEPS_PER_SEC(EXTRUDER_RPM, STEP_ANGLE, MICROSTEP_MULTIPLIER, GEAR_MULTIPLIER);
+AccelStepper g_stepper;
 
-  stepper.setMaxSpeed(MAX_STEPS_PER_SEC);
-  stepper.setSpeed(STEPS_PER_SEC);
+float rpm_to_steps_per_sec(float rpm, float step_angle_degrees, int microstep_multiplier = 1, float gear_ratio = 1.0) {
+  float steps_per_rev = 360.0 / step_angle_degrees;
+  steps_per_rev = steps_per_rev * microstep_multiplier;
+  steps_per_rev = steps_per_rev * gear_ratio;
 
-  pinMode(I_PIN_RUN, INPUT_PULLUP);
+  float steps_per_min = rpm * steps_per_rev;
+
+  float steps_per_sec = steps_per_min / 6.0;
+
+  return steps_per_sec;
 }
 
-void loop()
-{
-  if (digitalRead(I_PIN_RUN))
-  {
-    stepper.runSpeed();
-  }
+void setup() {
+  g_stepper = AccelStepper(AccelStepper::DRIVER, DO_STEP_PIN, DO_DIR_PIN);
+  // g_stepper.setEnablePin(O_PIN_ENABLE);
+  g_stepper.setPinsInverted(/* directionInvert */ STEPPER_INVERT_DIRECTION,
+                            /* stepInvert */ false,
+                            /* enableInvert */ true);
+
+  float steps_per_sec = rpm_to_steps_per_sec(EXTRUDER_RPM, STEP_ANGLE_DEGREES, MICROSTEP_MULTIPLIER, GEAR_RATIO);
+
+  g_stepper.setMaxSpeed(MAX_STEPS_PER_SEC);
+  g_stepper.setSpeed(steps_per_sec);
+
+  pinMode(DI_ROBOT_STEPPER_FORWARDS_PIN, INPUT_PULLUP);
 }
 
-int RPM_TO_STEPS_PER_SEC(int rpm, float step_angle, int microstep_multiplier, int gear_multiplier) {
-  int steps_per_rev = int(360 / step_angle);
-
-  int actual_steps_per_rev = steps_per_rev * microstep_multiplier * gear_multiplier;
-  int steps_per_min = rpm * actual_steps_per_rev;
-
-  return steps_per_min / 60;
+void loop() {
+  if (digitalRead(DI_ROBOT_STEPPER_FORWARDS_PIN))
+    g_stepper.runSpeed();
 }
